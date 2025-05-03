b4bec3d47f8761c49d6c64e99df83c3199931526
emptygxd <arsenii_rusin@mail.ru>
2025-05-03 17:40:07 +0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/<REPOSITORY_NAME>/',
+  base: '/testovoeCatViewer/',
   plugins: [react()],
 });
